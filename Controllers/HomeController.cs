3bf57adc6244d894d49913a67a818f6690642762
ctraore-hn <ctraore@hn-services.fr>
2023-04-13 11:27:27 +0200

    Ajoutez des fichiers projet.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using TPLOCAL1.Models;
+
+//L'énoncé du tp et le logo hn sont livrés dans le répertoire /ressources de la solution
+//--------------------------------------------------------------------------------------
+//Attention, le modèle MVC est un modèle dit de convention plutot que configuration, 
+//Le controller doit donc obligatoirement se nommer avec "Controller" à la fin!!!
+namespace TPLOCAL1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // méthode appelée par la routeur "naturellement"
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                //renvoie vers la vue Index (voir routage dans RouteConfig.cs)
+                return View();
+            else
+            {
+                //en fonction du paramètre id, on appelle les différentes pages
+                switch (id)
+                {
+                    case "ListeAvis":
+                        //reste à faire : coder la lecture du fichier xml fourni
+                        return View(id);
+                    case "Formulaire":
+                        //reste à faire : appeler la vue Formulaire avec le modèle de données vide
+                        
+                        return View(id);
+                    default:
+                        //renvoie vers Index (voir routage dans RouteConfig.cs)
+                        return View();
+                }
+            }
+        }
+
+
+        //méthode pour envoyer les données du formulaire vers la page de validation
+        [HttpPost]
+        public ActionResult ValidationFormulaire([Bind("Id,nom,prenom,sexe,adresse,codeP,ville,mail,dateDeb,typForm,cobolForm,objetForm")]
+        FormulaireModel formulaire)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(formulaire);
+            }
+            else 
+            {
+                return Redirect("/Home/Index/Formulaire");
+            }
+            //reste à faire : tester de si les champs du modele sont bien remplis
+            //s'ils ne sont pas bien remplis, afficher une erreur et rester sur la page formulaire
+            //sinon, appeler la page ValidationFormulaire avec les données remplies par l'utilisateur
+
+        }
+    }
+
+}
---
+++ Models/FormulaireModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace TPLOCAL1.Models
+{
+    public class FormulaireModel
+    {
+        public int Id;
+        [Required(ErrorMessage ="Please pick a name")]
+        public string nom { get; set; }
+        [Required]
+        public string prenom { get; set; }
+        [Required]
+        public string sexe { get; set; }
+        [Required]
+        public string adresse { get; set; }
+        [Required]
+        public string ville { get; set; }
+        [Required]
+        public string codeP { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string mail { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime dateDeb { get; set; }
+        [Required]
+        public string typForm { get; set; }
+        public string? cobolForm { get; set; }
+        public string? objetForm { get; set; }
+    
+ 
+    }
+}
